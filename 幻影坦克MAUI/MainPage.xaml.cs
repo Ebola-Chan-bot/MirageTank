f1b0eaa778516336ba79b358071b03d580349931
埃博拉酱 <vhtmfu@outlook.com>
2025-03-27 18:51:14 +0800

    20250327

--- 幻影坦克MAUI/MainPage.xaml.cs
+++ 幻影坦克MAUI/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using Microsoft.ML.OnnxRuntime;
 using Onnx;
 using SkiaSharp;
 using SkiaSharp.Views.Maui;
+using System.Threading.Tasks;
 
 namespace 幻影坦克MAUI
 {
@@ -16,16 +17,16 @@ namespace 幻影坦克MAUI
 		{
 			InitializeComponent();
 		}
-		private static byte[] 位图转字节(SKBitmap 位图对象, ushort 输出高度, ushort 输出宽度, ColorPicker 颜色)
+		private static byte[] 位图转字节(SKBitmap 位图对象, int 输出高度, int 输出宽度, ColorPicker 颜色)
 		{
 			SKBitmap 输出位图 = new(输出宽度, 输出高度, SKColorType.Rgba8888, SKAlphaType.Unpremul);
 			using (SKCanvas 画布 = new(输出位图))
 			{
 				画布.Clear(颜色.PickedColor.ToSKColor());
-				输出高度 = (ushort)Math.Min(输出高度, 位图对象.Height * 输出宽度 / 位图对象.Width);
-				输出宽度 = (ushort)Math.Min(输出宽度, 位图对象.Width * 输出高度 / 位图对象.Height);
-				ushort 左偏移 = (ushort)((输出位图.Width - 输出宽度) / 2);
-				ushort 上偏移 = (ushort)((输出位图.Height - 输出高度) / 2);
+				输出高度 = Math.Min(输出高度, 位图对象.Height * 输出宽度 / 位图对象.Width);
+				输出宽度 = Math.Min(输出宽度, 位图对象.Width * 输出高度 / 位图对象.Height);
+				float 左偏移 = ((输出位图.Width - 输出宽度) / 2);
+				float 上偏移 = ((输出位图.Height - 输出高度) / 2);
 				画布.DrawBitmap(位图对象, new SKRect(左偏移, 上偏移, 输出宽度 + 左偏移, 输出高度 + 上偏移));
 			}
 			return 输出位图.Bytes;
@@ -40,31 +41,75 @@ namespace 幻影坦克MAUI
 			输入变量 数组输入 = new(张量.名称, TensorProto.Types.DataType.Float, [-1, -1, -1, -1]);
 			中间变量 中间最大 = 变量.Where(最大输入 > 255f, 最大输入, 255f);
 			中间变量 中间最小 = 变量.Where(最小输入 < 0f, 最小输入, 0f);
+			//string 输出变量名 = 变量.分配标识符();
 			return 全局最小.IsInf(false, true).If
 			(
 				输出变量.生成计算图(最大输入.IsInf(true, false).If
 				(
 					输出变量.生成计算图(((((数组输入 / 255f - 0.5f) * MathF.PI).Atan() / MathF.PI + 0.5f) * 255f).Identity(TensorProto.Types.DataType.Float, [-1, -1, -1, -1])),
 					输出变量.生成计算图((65025f / (255f + 最大输入 - 数组输入)).Identity(TensorProto.Types.DataType.Float, [-1, -1, -1, -1]))
-				).Single().Identity(TensorProto.Types.DataType.Float, [-1, -1, -1, -1])),
+				).Single().Identity( TensorProto.Types.DataType.Float, [-1, -1, -1, -1])),
 				输出变量.生成计算图(最大输入.IsInf(true, false).If
 				(
-					输出变量.生成计算图((65025f / (最小输入 - 255f - 数组输入) + 255f).Identity(TensorProto.Types.DataType.Float, [-1, -1, -1, -1])),
+					输出变量.生成计算图((65025f / (最小输入 - 255f - 数组输入) + 255f).Identity( TensorProto.Types.DataType.Float, [-1, -1, -1, -1])),
 					输出变量.生成计算图((最大输入 <= 255f & 最小输入 >= 0).If
 					(
-						输出变量.生成计算图(数组输入.Identity(Guid.NewGuid().ToString(), TensorProto.Types.DataType.Float, [-1, -1, -1, -1])),
-						输出变量.生成计算图((255f / (中间最大 - 中间最小) * (数组输入 - 中间最小)).Identity(TensorProto.Types.DataType.Float, [-1, -1, -1, -1]))
-					).Single().Identity(TensorProto.Types.DataType.Float, [-1, -1, -1, -1]))
-				).Single().Identity(TensorProto.Types.DataType.Float, [-1, -1, -1, -1]))
+						输出变量.生成计算图(数组输入.Identity(变量.分配标识符(), TensorProto.Types.DataType.Float, [-1, -1, -1, -1])),
+						输出变量.生成计算图((255f / (中间最大 - 中间最小) * (数组输入 - 中间最小)).Identity( TensorProto.Types.DataType.Float, [-1, -1, -1, -1]))
+					).Single().Identity( TensorProto.Types.DataType.Float, [-1, -1, -1, -1]))
+				).Single().Identity( TensorProto.Types.DataType.Float, [-1, -1, -1, -1]))
 			).Single();
 		}
 		private SKData 预览图;
+#if DEBUG
+		private static void 验证模型(params 中间变量[]要验证的输出)
+		{
+			GraphProto 计算图 = 输出变量.生成计算图(from 中间变量 V in 要验证的输出 select V.Identity(TensorProto.Types.DataType.Uint8, [-1, -1, -1, -1]));
+			ModelProto 模型 = new()
+			{
+				IrVersion = 10,
+				OpsetImport = { new OperatorSetIdProto() { Version = 22 } },
+				Graph = 计算图
+			};
+			模型.WriteToFile(模型路径);
+			new InferenceSession(模型.ToByteArray());
+		}
+#endif
+		private static byte[] 更新模型会话(long 输出宽度,long 输出高度)
+		{
+			中间变量 表里图 = new 输入变量("表里图", TensorProto.Types.DataType.Uint8, [4, -1, -1, 2]).Cast(TensorProto.Types.DataType.Float);
+			中间变量 背景色 = new 输入变量("背景色", TensorProto.Types.DataType.Uint8, [3, 1, 1, 2]).Cast(TensorProto.Types.DataType.Float);
+			中间变量[] 表里背景色 = [.. 背景色.Split(3, 2, new 常量([1, 1]))];
+			中间变量[] 颜色透明通道 = [.. 表里图.Split(0, 2, new 常量([3, 1]))];
+			表里图 = ((255f - 颜色透明通道[1]) * 背景色 + 颜色透明通道[1] * 颜色透明通道[0]) / 255f;
+			背景色 = 表里背景色[0] - 表里背景色[1];
+			常量 三色权重 = new([0.299f, 0.587f, 0.114f], [3]);
+			中间变量 加权背景差 = 背景色 * 三色权重;
+			中间变量[] 表里图拆分 = [.. 表里图.Split(3, 2, new 常量([1, 1]))];
+			表里图 = (加权背景差 * 三色权重 * (表里图拆分[0] - 表里图拆分[1])).ReduceSum(0) / (加权背景差 * 加权背景差).Sum();
+			表里图 = 变量.Where(表里图.IsNaN(), 0, 表里图) * 背景色;
+			中间变量 表里色和 = 表里图拆分[0] + 表里图拆分[1];
+			表里色和 = 范围放缩(变量.Concat(3, (表里色和 + 表里图) / 2f, (表里色和 - 表里图) / 2f));
+			表里图拆分 = [.. 表里色和.Split(3, 2, new 常量([1, 1]))];
+			颜色透明通道[1] = 范围放缩(255f * (1f - (表里图拆分[0] - 表里图拆分[1]) / 背景色));
+			颜色透明通道[1] = 变量.Where(颜色透明通道[1].IsNaN(), 255f, 颜色透明通道[1]).ReduceMean(0);
+			背景色 = 表里背景色[0] + 表里背景色[1];
+			表里图 = 范围放缩((255f * (表里图拆分[0] + 表里图拆分[1] - 背景色) / 颜色透明通道[1] + 背景色) / 2);
+			ModelProto 模型原型 = new()
+			{
+				IrVersion = 10,
+				OpsetImport = { new OperatorSetIdProto() { Version = 22 } },
+				Graph = 输出变量.生成计算图(变量.Concat(0, 表里图, 颜色透明通道[1]).Cast(TensorProto.Types.DataType.Uint8).Identity("幻影坦克", TensorProto.Types.DataType.Uint8, [4, 输出宽度, 输出高度]))
+			};
+			模型原型.WriteToFile(模型路径);
+			return 模型原型.ToByteArray();
+		}
 		private void Generate_Clicked(object sender, EventArgs e)
 		{
 			SKBitmap 表图对象 = SKBitmap.Decode(表图流);
 			SKBitmap 里图对象 = SKBitmap.Decode(里图流);
-			ushort 输出高度 = (ushort)Math.Max(表图对象.Height, 里图对象.Height);
-			ushort 输出宽度 = (ushort)Math.Max(表图对象.Width, 里图对象.Width);
+			int 输出高度 = Math.Max(表图对象.Height, 里图对象.Height);
+			int 输出宽度 = Math.Max(里图对象.Width, 里图对象.Width);
 			SessionOptions 会话选项 = new();
 #if ANDROID
 			会话选项.AppendExecutionProvider_Nnapi();
@@ -81,39 +126,16 @@ namespace 幻影坦克MAUI
 #endif
 			InferenceSession 推理会话;
 			if (File.Exists(模型路径))
-				推理会话 = new(模型路径, 会话选项);
-			else
-			{
-				中间变量 表里图 = new 输入变量("表里图", TensorProto.Types.DataType.Uint8, [4, -1, -1, 2]).Cast(TensorProto.Types.DataType.Float);
-				中间变量 背景色 = new 输入变量("背景色", TensorProto.Types.DataType.Uint8, [3, 1, 1, 2]).Cast(TensorProto.Types.DataType.Float);
-				中间变量 透明通道 = 表里图.Slice(3, 4, 0);
-				中间变量 表里亮度 = ((255f - 透明通道) * 背景色 + 透明通道 * 表里图.Slice(0, 3, 0)) / 255f;
-				中间变量 表背景色 = 背景色.Slice(0, 1, 3);
-				中间变量 里背景色 = 背景色.Slice(1, 2, 3);
-				中间变量 背景色差 = 表背景色 - 里背景色;
-				常量 三色权重 = new([0.299f, 0.587f, 0.114f], [3]);
-				中间变量 加权背景差 = 背景色差 * 三色权重;
-				中间变量 表图 = 表里亮度.Slice(0, 1, 3);
-				中间变量 里图 = 表里亮度.Slice(1, 2, 3);
-				中间变量 表里色差 = (加权背景差 * 三色权重 * (表图 - 里图)).ReduceSum(0) / (加权背景差 * 加权背景差).Sum();
-				表里色差 = 变量.Where(表里色差.IsNaN(), 0, 表里色差) * 背景色差;
-				中间变量 表里色和 = 表图 + 里图;
-				表里色和 = 范围放缩(变量.Concat(3, (表里色和 + 表里色差) / 2f, (表里色和 - 表里色差) / 2f));
-				表图 = 表里色和.Slice(0, 1, 3);
-				里图 = 表里色和.Slice(1, 2, 3);
-				透明通道 = 范围放缩(255f * (1f - (表图 - 里图) / 背景色差));
-				透明通道 = 变量.Where(透明通道.IsNaN(), 255f, 透明通道).ReduceMean(0);
-				中间变量 背景色和 = 表背景色 + 里背景色;
-				中间变量 颜色通道 = 范围放缩((255f * (表图 + 里图 - 背景色和) / 透明通道 + 背景色和) / 2);
-				ModelProto 模型原型 = new()
+				try
 				{
-					IrVersion = (long)Onnx.Version.IrVersion,
-					OpsetImport = { new OperatorSetIdProto() { Version = 22 } },
-					Graph = 输出变量.生成计算图(变量.Concat(0, 颜色通道, 透明通道).Cast(TensorProto.Types.DataType.Uint8).Identity("幻影坦克", TensorProto.Types.DataType.Uint8, [4, 输出宽度, 输出高度]))
-				};
-				模型原型.WriteToFile(模型路径);
-				推理会话 = new(模型原型.ToByteArray(), 会话选项);
-			}
+					推理会话 = new(模型路径, 会话选项); 
+				}
+				catch (OnnxRuntimeException)
+				{
+					推理会话 = new(更新模型会话(输出宽度,输出高度), 会话选项);
+				}
+			else
+				推理会话 = new(更新模型会话(输出宽度,输出高度), 会话选项);
 			SKBitmap 幻影坦克 = new();
 			幻影坦克.InstallPixels(new SKImageInfo(输出宽度, 输出高度, SKColorType.Rgba8888, SKAlphaType.Unpremul), SKData.CreateCopy(推理会话.Run(new RunOptions(), new Dictionary<string, OrtValue>
 			{
@@ -128,13 +150,18 @@ namespace 幻影坦克MAUI
 			明场预览.Source = ImageSource.FromStream(() => 预览图.AsStream());
 			暗场预览.Source = ImageSource.FromStream(() => 预览图.AsStream());
 		}
-
+		private static ImageSource 拷贝流图源(Stream 源图流)
+		{
+			MemoryStream 内存流 = new();
+			源图流.CopyTo(内存流);
+			内存流.Position = 0;
+			源图流.Position = 0;
+			return ImageSource.FromStream(() => 内存流);
+		}
 		private async void ContentPage_Loaded(object sender, EventArgs e)
 		{
-			表图流 = await FileSystem.OpenAppPackageFileAsync($"surface_raw.jpg");
-			里图流 = await FileSystem.OpenAppPackageFileAsync($"hidden_raw.jpg");
-			表图.Source = ImageSource.FromStream(() => 表图流);
-			里图.Source = ImageSource.FromStream(() => 里图流);
+			表图.Source = 拷贝流图源(表图流 = await FileSystem.OpenAppPackageFileAsync($"surface_raw.jpg"));
+			里图.Source = 拷贝流图源(里图流 = await FileSystem.OpenAppPackageFileAsync($"hidden_raw.jpg"));
 		}
 		readonly PickOptions options = new()
 		{
@@ -144,21 +171,14 @@ namespace 幻影坦克MAUI
 		private async void 表图_Tapped(object sender, TappedEventArgs e)
 		{
 			FileResult? result = await FilePicker.Default.PickAsync(options);
-
 			if (result != null)
-			{
-				表图流 = await result.OpenReadAsync();
-				表图.Source = ImageSource.FromStream(() => 表图流);
-			}
+				表图.Source = 拷贝流图源(表图流 = await result.OpenReadAsync());
 		}
 		private async void 里图_Tapped(object sender, TappedEventArgs e)
 		{
 			FileResult? result = await FilePicker.Default.PickAsync(options);
 			if (result != null)
-			{
-				里图流 = await result.OpenReadAsync();
-				里图.Source = ImageSource.FromStream(() => 里图流);
-			}
+				里图.Source = 拷贝流图源(里图流 = await result.OpenReadAsync());
 		}
 	}
 }
--- 幻影坦克MAUI/Onnx运算原型.cs
+++ 幻影坦克MAUI/Onnx运算原型.cs
@@ -35,32 +35,44 @@ namespace Onnx
 			});
 			return new(计算图);
 		}
+		private static byte 标识符计数 = 0;
+		public static string 分配标识符()
+		{
+			return "_埃博拉酱_" + 标识符计数++;
+		}
 		public static IEnumerable<中间变量> 运算(string 运算名称, IEnumerable<变量> 输入, IEnumerable<AttributeProto> 特性, int 输出个数)
 		{
-			IEnumerable<string> 变量名 = Enumerable.Range(0, 输出个数).Select(_ => Guid.NewGuid().ToString());
-			return 变量名.Select(名称 => new 中间变量(new()
+			string[] 变量名 = Enumerable.Range(0, 输出个数).Select(_ => 分配标识符()).ToArray();
+			//必须先ToArray确定下来，不能保留IEnumerable，否则每次都会获取新的标识符，不能重用
+
+			GraphProto 模板 = new()
 			{
-				Name = 名称,
 				Input = { 输入.SelectMany(值 => 值.计算图.Input).Distinct() },
 				Initializer = { 输入.SelectMany(值 => 值.计算图.Initializer).Distinct() },
-				Node = { 输入.SelectMany(值 => 值.计算图.Node),new NodeProto()
+				Node = { 输入.SelectMany(值 => 值.计算图.Node).Distinct(),new NodeProto()
 				{
 					OpType = 运算名称,
 					Input = { 输入.Select(值=>值.计算图.Name) },
 					Output = { 变量名 },
 					Attribute = { 特性 }
 				} }
-			}));
+			};
+			foreach (string 名称 in 变量名)
+			{
+				GraphProto 实例 = 模板.Clone();
+				实例.Name = 名称;
+				yield return new(实例);
+			}
 		}
 		public static 中间变量 运算(string 运算名称, IEnumerable<变量> 输入, IEnumerable<AttributeProto> 特性)
 		{
-			string 变量名 = Guid.NewGuid().ToString();
+			string 变量名 = 分配标识符();
 			return new(new()
 			{
 				Name = 变量名,
 				Input = { 输入.SelectMany(值 => 值.计算图.Input).Distinct() },
 				Initializer = { 输入.SelectMany(值 => 值.计算图.Initializer).Distinct() },
-				Node = { 输入.SelectMany(值 => 值.计算图.Node),new NodeProto()
+				Node = { 输入.SelectMany(值 => 值.计算图.Node).Distinct(),new NodeProto()
 				{
 					OpType = 运算名称,
 					Input = { 输入.Select(值=>值.计算图.Name) },
@@ -71,13 +83,13 @@ namespace Onnx
 		}
 		public static 中间变量 运算(string 运算名称, IEnumerable<变量> 输入)
 		{
-			string 变量名 = Guid.NewGuid().ToString();
+			string 变量名 = 分配标识符();
 			return new(new()
 			{
 				Name = 变量名,
 				Input = { 输入.SelectMany(值 => 值.计算图.Input).Distinct() },
 				Initializer = { 输入.SelectMany(值 => 值.计算图.Initializer).Distinct() },
-				Node = { 输入.SelectMany(值 => 值.计算图.Node),new NodeProto()
+				Node = { 输入.SelectMany(值 => 值.计算图.Node).Distinct(),new NodeProto()
 				{
 					OpType = 运算名称,
 					Input = { 输入.Select(值=>值.计算图.Name) },
@@ -159,6 +171,7 @@ namespace Onnx
 				}
 			]);
 		}
+		//两个子图的输出数量必须相同
 		public IEnumerable<中间变量> If(GraphProto 如果真, GraphProto 如果假)
 		{
 			return 运算("If", [this],
@@ -230,7 +243,18 @@ namespace Onnx
 		{
 			return 运算("ReduceMean", [this, 轴]);
 		}
-
+		public IEnumerable<中间变量> Split(int 轴, int 拆分个数, 变量 拆分长度)
+		{
+			return 运算("Split", [this, 拆分长度],
+			[
+				new()
+				{
+					Name = "axis",
+					Type = AttributeProto.Types.AttributeType.Int,
+					I = 轴
+				}
+			], 拆分个数);
+		}
 	}
 	class 输出变量:变量
 	{
@@ -245,20 +269,21 @@ namespace Onnx
 				计算图.Name = value;
 			}
 		}
-		//仅需传入所有输出变量即可生成最小计算模型
-		public static GraphProto 生成计算图(params 输出变量[] 所有输出)
+		//仅需传入所有输出变量即可生成最小计算模型。输出图的所有输出变量名与输入的所有输出变量名相同，但重复的输出变量名只保留一个，且不保证顺序。
+		public static GraphProto 生成计算图(IEnumerable<输出变量> 所有输出)
 		{
-			GraphProto 返回值 = new()
+			return new()
 			{
 				Input = { 所有输出.SelectMany(输出 => 输出.计算图.Input).Distinct() },
 				Output = { 所有输出.SelectMany(输出 => 输出.计算图.Output).Distinct() },
 				Initializer = { 所有输出.SelectMany(输出 => 输出.计算图.Initializer).Distinct() },
+				Node = { 所有输出.SelectMany(输出 => 输出.计算图.Node).Distinct() },
 			};
-			foreach (输出变量 输出 in 所有输出)
-				foreach (NodeProto 节点 in 输出.计算图.Node)
-					if (!返回值.Node.Contains(节点))
-						返回值.Node.Add(节点);
-			return 返回值;
+		}
+		//仅需传入所有输出变量即可生成最小计算模型。输出图的所有输出变量名与输入的所有输出变量名相同，但重复的输出变量名只保留一个，且不保证顺序。
+		public static GraphProto 生成计算图(params 输出变量[] 所有输出)
+		{
+			return 生成计算图(所有输出);
 		}
 	}
 	class 输入变量(string 输入名称, TensorProto.Types.DataType 数据类型, IEnumerable<long> 各维尺寸) : 变量(new()
@@ -294,42 +319,50 @@ namespace Onnx
 		})
 		{ }
 		static readonly Dictionary<object, 常量> 常量表 = [];
+		public 常量(IEnumerable<long> 值, IEnumerable<long> 各维尺寸) : base(new Func<GraphProto>(() =>
+		{
+			string 名称 = 分配标识符();
+			return new()
+			{
+				Name = 名称,
+				Initializer = { new TensorProto()
+				{
+					Name =名称,
+					Dims = { 各维尺寸 },
+					DataType = (int)TensorProto.Types.DataType.Int64,
+					Int64Data = { 值 }
+				} }
+			};
+		})())
+		{ }
+		public 常量(IEnumerable<long> 值) : this(值, [值.LongCount()])
+		{ }
 		public static implicit operator 常量(long 值)
 		{
-			if (常量表.ContainsKey(值))
-				return 常量表[值];
-			return 常量表[值] = new(new TensorProto()
-			{
-				Name = Guid.NewGuid().ToString(),
-				Dims = { 1 },
-				DataType = (int)TensorProto.Types.DataType.Int64,
-				Int64Data = { 值 }
-			});
+			return 常量表.ContainsKey(值) ? 常量表[值] : (常量表[值] = new([值]));
 		}
+		public 常量(IEnumerable<float> 值, IEnumerable<long> 各维尺寸) : base(new Func<GraphProto>(() =>
+		{
+			string 名称 = 分配标识符();
+			return new()
+			{
+				Name = 名称,
+				Initializer = { new TensorProto()
+				{
+					Name =名称,
+					Dims = { 各维尺寸 },
+					DataType = (int)TensorProto.Types.DataType.Float,
+					FloatData = { 值 }
+				} }
+			};
+		})())
+		{ }
+		public 常量(IEnumerable<float> 值) : this(值, [值.LongCount()])
+		{ }
 		public static implicit operator 常量(float 值)
 		{
-			if (常量表.ContainsKey(值))
-				return 常量表[值];
-			return 常量表[值] = new(new TensorProto()
-			{
-				Name = Guid.NewGuid().ToString(),
-				Dims = { 1 },
-				DataType = (int)TensorProto.Types.DataType.Float,
-				FloatData = { 值 }
-			});
+			return 常量表.ContainsKey(值) ? 常量表[值] : (常量表[值] = new([值]));
 		}
-		public 常量(IEnumerable<float> 值, IEnumerable<long> 各维尺寸) : base(new()
-		{
-			Name = Guid.NewGuid().ToString(),
-			Initializer = { new TensorProto()
-			{
-				Name = Guid.NewGuid().ToString(),
-				Dims = { 各维尺寸 },
-				DataType = (int)TensorProto.Types.DataType.Float,
-				FloatData = { 值 }
-			} }
-		})
-		{ }
 		public override string 名称
 		{
 			get => 计算图.Name;
@@ -346,7 +379,6 @@ namespace Onnx
 		internal 中间变量(GraphProto 计算图) : base(计算图) { }
 		public override 输出变量 Identity(string 输出名称, TensorProto.Types.DataType 数据类型, IEnumerable<long> 各维尺寸)
 		{
-			计算图.Name = 输出名称;
 			计算图.Output.Add(new ValueInfoProto()
 			{
 				Name = 输出名称,
@@ -360,8 +392,11 @@ namespace Onnx
 				}
 			});
 			RepeatedField<string> 最终输出 = 计算图.Node.Last().Output;
-			最终输出.Remove(计算图.Name);
-			最终输出.Add(输出名称);
+			最终输出[最终输出.IndexOf(计算图.Name)] = 输出名称;
+
+			//计算图.Name必须最后设置，因为前面还用到原始值
+			计算图.Name = 输出名称;
+
 			return new(计算图);
 		}
 		//取得和此中间变量同名的输出变量。那之后，此中间变量将不再可用。
